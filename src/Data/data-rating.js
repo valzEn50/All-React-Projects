01a7597bf93795357645970d155657035c8b23f7
valzEn50 <vnwachukwu15@gmail.com>
2026-04-06 07:10:42 +0100

    confirm image source

--- src/Data/data-rating.js
+++ src/Data/data-rating.js
@@ -4,121 +4,121 @@ const dataRating = [
         id: 1,
         name: "Img 1",
         title: "Image title",
-        source: "../src/assets/images/img1.jpg"
+        source: "img1.jpg"
     },
     {
         id: 2,
         name: "Img 2",
         title: "Image title",
-        source: "./src/assets/images/img2.jpg"
+        source: "img2.jpg"
     },
     {
         id: 3,
         name: "Img 3",
         title: "Image title",
-        source: "./src/assets/images/img3.jpg"
+        source: "img3.jpg"
     },
     {
         id: 4,
         name: "Img 4",
         title: "Image title",
-        source: "./src/assets/images/img4.jpg"
+        source: "img4.jpg"
     },
     {
         id: 5,
         name: "Img 5",
         title: "Image title",
-        source: "./src/assets/images/img5.jpg"
+        source: "img5.jpg"
     },
     {
         id: 6,
         name: "Img 6",
         title: "Image title",
-        source: "./src/assets/images/img6.jpg"
+        source: "img6.jpg"
     },
     {
         id: 7,
         name: "Img 7",
         title: "Image title",
-        source: "./src/assets/images/img7.jpg"
+        source: "img7.jpg"
     },
     {
         id: 8,
         name: "Img 8",
         title: "Image title",
-        source: "./src/assets/images/img8.jpg"
+        source: "img8.jpg"
     },
     {
         id: 9,
         name: "Img 9",
         title: "Image title",
-        source: "./src/assets/images/img9.jpg"
+        source: "img9.jpg"
     },
     {
         id: 10,
         name: "Img 10",
         title: "Image title",
-        source: "./src/assets/images/img10.jpg"
+        source: "img10.jpg"
     },
     {
         id: 11,
         name: "Img 11",
         title: "Image title",
-        source: "./src/assets/images/img11.jpg"
+        source: "img11.jpg"
     },
     {
         id: 12,
         name: "Img 12",
         title: "Image title",
-        source: "./src/assets/images/img12.jpg"
+        source: "img12.jpg"
     },
     {
         id: 13,
         name: "Img 13",
         title: "Image title",
-        source: "./src/assets/images/img13.jpg"
+        source: "img13.jpg"
     },
     {
         id: 14,
         name: "Img 14",
         title: "Image title",
-        source: "./src/assets/images/img14.jpg"
+        source: "img14.jpg"
     },
     {
         id: 15,
         name: "Img 15",
         title: "Image title",
-        source: "./src/assets/images/img15.jpg"
+        source: "img15.jpg"
     },
     {
         id: 16,
         name: "Img 16",
         title: "Image title",
-        source: "./src/assets/images/img16.jpg"
+        source: "img16.jpg"
     },
     {
         id: 17,
         name: "Img 17",
         title: "Image title",
-        source: "./src/assets/images/img17.jpg"
+        source: "img17.jpg"
     },
     {
         id: 18,
         name: "Img 18",
         title: "Image title",
-        source: "./src/assets/images/img18.jpg"
+        source: "img18.jpg"
     },
     {
         id: 19,
         name: "Img 19",
         title: "Image title",
-        source: "./src/assets/images/img19.jpg"
+        source: "img19.jpg"
     },
     {
         id: 20,
         name: "Img 20",
         title: "Image title",
-        source: "./src/assets/images/img20.jpg"
+        source: "img20.jpg"
     }
 ]
 
